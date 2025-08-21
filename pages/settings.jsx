28ca2d19eb5cc5e29ac5ae37662a13d45a1f819f
ikspass <moolingtoon@mail.ru>
2025-08-21 15:36:29 +0300

    start settings, dashboard, stats

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,9 +1,24 @@
 import MainContainer from "../components/mainContainer";
+import { useState, useEffect } from 'react';
+import { getCategories } from "../utils/api";
+
 
 const Settings = () => {
+  const [limit, setLimit] = useState(0);
+  const [budget, setBudget] = useState(0)
+
+  const saveSettings = () => {
+    alert('Изменения сохранены')
+  }
+
   return (
     <MainContainer>
       <h1>Настройки лимита</h1>
+      <p>Лимит</p>
+      <input value={limit} onChange={e => setLimit(e.target.value)} type="text" />
+      <p>Бюджет</p>
+      <input value={budget} onChange={e => setBudget(e.target.value)} type="text" />
+      <button onClick={saveSettings}>Ок</button>
     </MainContainer>
   )
 }
